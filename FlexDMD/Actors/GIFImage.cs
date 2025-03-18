e143fb8b392ece08e7957057c04383981a6dbc7a
gustavoalara <gustavoa.laraheredero@gmail.com>
2025-03-18 11:22:21 +0100

    Update GIFImage.cs

--- FlexDMD/Actors/GIFImage.cs
+++ FlexDMD/Actors/GIFImage.cs
@@ -1,4 +1,20 @@
-﻿using FlexDMD.Actors;
+﻿/* Copyright 2025 Gustavo A. Lara
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+
+
+using FlexDMD.Actors;
 using NLog;
 using ImageMagick;
 using System;
@@ -158,12 +174,11 @@ namespace FlexDMD
                     Layout.Align(Alignment, w, h, Width, Height, out float x, out float y);
                     graphics.DrawImage(frameToDraw, (int)(X + x), (int)(Y + y), (int)w, (int)h);
 
-                    frameToDraw.Dispose(); // Liberar el nuevo Bitmap
+                    frameToDraw.Dispose(); // Free the new Bitmap
                 }
                 catch (Exception ex)
                 {
-                    log.Error(ex, "Error drawing GIF/APNG frame.");
-                    // Manejar el error adecuadamente
+                    log.Error(ex, "Error drawing GIF frame.");
                 }
             }
         }
